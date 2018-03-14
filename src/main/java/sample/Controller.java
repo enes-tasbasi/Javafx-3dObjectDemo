c475cf4486f2f78dfaedea8b4a57ae649ffa0c30
Enes Tasbasi <enestasbasi@outlook.com>
2018-03-13 22:11:59 -0400

    change the project to maven

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.PointLight;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
@@ -30,19 +31,15 @@ public class Controller {
     public TextField tf1, tf2, tf3;
 
     @FXML
+    public HBox hbox;
+
+    @FXML
     public GridPane gridPane;
 
     @FXML
     public StackPane stackPane;
 
-    @FXML
-    public MenuBar menuBar;
-
-    @FXML
-    public Menu fileMenu;
-
-    @FXML
-    public MenuItem closeMenuItem;
+    public MenuItem rotateMenuItem;
 
     public MenuItem deleteMenuItem;
 
@@ -63,7 +60,7 @@ public class Controller {
 
         contextMenu = new ContextMenu();
 
-        MenuItem rotateMenuItem = new MenuItem("Rotate");
+        rotateMenuItem = new MenuItem("Rotate");
         rotateMenuItem.setOnAction(e -> {
             TextField rotateTextField = new TextField();
             rotateTextField.setPromptText("Degrees");
@@ -91,13 +88,11 @@ public class Controller {
         deleteMenuItem.setOnAction(event -> {
 
             stackPane.getChildren().remove(currentRightClick);
-            shapes.remove(currentRightClick);
 
         });
 
         contextMenu.getItems().addAll(rotateMenuItem, deleteMenuItem);
 
-        closeMenuItem.setOnAction(event -> Main.close());
 
     }
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,19 +2,20 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.PerspectiveCamera;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/sample.fxml"));
+
         Parent root = loader.load();
 
         Controller myController = loader.getController();
+
         Scene scene = new Scene(root);
 
         myController.initialize();
@@ -27,7 +28,7 @@ public class Main extends Application {
         camera.setFieldOfView(30);
 
         scene.setCamera(camera);
-        scene.getStylesheets().add("/sample/main.css");
+        scene.getStylesheets().add("/css/main.css");
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
@@ -36,9 +37,5 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public static void close() {
-        System.exit(0);
     }
 }
